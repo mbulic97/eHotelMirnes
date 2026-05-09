95516e306df8ece28d5133b894242912606a661a
mbulic97 <mirnesbulic32@gmail.com>
2026-05-09 14:07:41 +0200

    feat: add UserDTO

---
+++ backend/src/main/java/com/eHotelMirnes/backend/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package com.eHotelMirnes.backend.dto;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDTO {
+    private Long id;
+    private String email;
+    private String name;
+    private String phoneNumber;
+    private String role;
+    private List<BookingDTO> booking = new ArrayList<>();
+}
