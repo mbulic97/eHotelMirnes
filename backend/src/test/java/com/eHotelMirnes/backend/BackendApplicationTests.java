4d7b664d9829ff29e4b28616cd10c56d53314df7
mbulic97 <mirnesbulic32@gmail.com>
2026-05-15 18:12:17 +0200

    test: add UserRepository and entity unit tests

--- backend/src/test/java/com/eHotelMirnes/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/eHotelMirnes/backend/BackendApplicationTests.java
@@ -1,13 +1,34 @@
 package com.eHotelMirnes.backend;
 
+import com.eHotelMirnes.backend.entity.Booking;
+import com.eHotelMirnes.backend.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+//@SpringBootTest
 class BackendApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
+	}*/
+	@Test
+	void shouldReturnUsername() {
+		User user = new User();
+		user.setEmail("mirnesbulic@gmail.com");
+
+		assertEquals("mirnesbulic@gmail.com", user.getUsername());
+
+	}
+	@Test
+	void shouldCalculateTotalGuests() {
+		Booking booking = new Booking();
+		booking.setNumOfAdults(2);
+		booking.setNumOfChildren(1);
+
+		assertEquals(3, booking.getTotalNumOfGuest());
 	}
 
 }
---
+++ backend/src/test/java/com/eHotelMirnes/backend/UserRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.eHotelMirnes.backend;
+import com.eHotelMirnes.backend.entity.User;
+import com.eHotelMirnes.backend.repo.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+//@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserRepositoryTest{
+    @Autowired
+    private UserRepository userRepository;
+    @Test
+    void shouldSaveUserAndGenerateId() {
+        User user = new User();
+        user.setEmail("test1@gmail.com");
+        user.setName("Test1");
+        user.setPassword("123");
+        user.setPhoneNumber("056543165");
+        user.setRole("User");
+        User savedUser = userRepository.save(user);
+
+        assertNotNull(savedUser.getId());
+    }
+}
