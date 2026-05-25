805e0a5c675dee7b17cf9b11347c1a2aa90a236f
mbulic97 <mirnesbulic32@gmail.com>
2026-05-25 12:39:04 +0200

    feat: add login/register navbar and logout support

--- frontend/src/component/common/Navbar.jsx
+++ frontend/src/component/common/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 import './NavBar.css'
+import ApiService from '../../service/ApiService'
 
 function Navbar(){
+    const isAuthenticated = ApiService.isAuthenticated();
+    const navigate = useNavigate();
+
+    const handleLogout = () =>{
+        const isLogout = window.confirm('Are you sure you want to logout this user?');
+        if(isLogout){
+            ApiService.logout()
+            navigate('/home');
+        }
+    };
     return (
         <nav className="navbar">
             <div className="navbar-brand">
@@ -12,6 +23,10 @@ function Navbar(){
                 <li><NavLink to="/home" className={({isActive}) => isActive ? "active" : ""}>Home</NavLink></li>
                 <li><NavLink to="/rooms" className={({isActive}) => isActive ? "active" : ""}>Rooms</NavLink></li>
                 <li><NavLink to="/find-booking" className={({isActive}) => isActive ? "active" : ""}>Find My Booking</NavLink></li>
+
+                {!isAuthenticated && <li><NavLink to="/login" className={({isActive}) => isActive ? "active" : ""}>Login</NavLink></li>}
+                {!isAuthenticated && <li><NavLink to="/register" className={({isActive}) => isActive ? "active" : ""}>Register</NavLink></li>}
+                {isAuthenticated && <li className="logout" onClick={handleLogout}>Logout</li>}
             </ul>
         </nav>
     )
--- frontend/src/service/ApiService.js
+++ frontend/src/service/ApiService.js
@@ -1,7 +1,17 @@
 import axios from "axios"
 
 export default class ApiService {
+
     static BASE_URL = "http://localhost:8080"
+
+    static getHeader() {
+        const token = localStorage.getItem("token");
+        return {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json"
+        };
+    }
+
     static async registerUser(registration) {
         const response = await axios.post(`${this.BASE_URL}/auth/register`, registration)
         return response.data
@@ -10,5 +20,14 @@ export default class ApiService {
     static async loginUser(loginDetails) {
         const response = await axios.post(`${this.BASE_URL}/auth/login`, loginDetails)
         return response.data
+    }
+    
+    static logout(){
+        localStorage.removeItem('token')
+        localStorage.removeItem('role')
+    }
+    static isAuthenticated() {
+        const token = localStorage.getItem('role')
+        return !!token
     }
 }
