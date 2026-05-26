027dc0f27236c6d9a1b82c9cdbb1d64f247e2a09
mbulic97 <mirnesbulic32@gmail.com>
2026-05-26 16:45:45 +0200

    style: update footer with social links and icons

--- frontend/src/component/common/Footer.jsx
+++ frontend/src/component/common/Footer.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import './Footer.css'
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 const Footer = () => {
     return (
         <footer className="footer">
+            <div className="footer-social">
+
+                <a href="https://github.com/mbulic97" target="_blank" rel="noreferrer">
+                    <FaGithub /> My Profile
+                </a>
+
+                <a href="https://github.com/mbulic97/eHotelMirnes" target="_blank" rel="noreferrer">
+                    <FaGithub /> Project
+                </a>
+
+                <a href="https://www.linkedin.com/in/mirnes-buli%C4%87-0b74771a4/" target="_blank" rel="noreferrer">
+                    <FaLinkedin /> LinkedIn
+                </a>
+
+                <a href="mailto:mirnes-bulic@hotmail.com">
+                    <FaEnvelope /> Email
+                </a>
+
+            </div>
             <div className="footer-bottom">
                 <p>May 2026 eHotelMirnes. All rights reserved.</p>
             </div>
