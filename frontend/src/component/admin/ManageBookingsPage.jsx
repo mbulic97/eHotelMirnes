1471645dac8eddf437e823394e10876cefce898f
mbulic97 <mirnesbulic32@gmail.com>
2026-09-22 23:10:07 +0200

    feat: add booking management for frontend

---
+++ frontend/src/component/admin/ManageBookingsPage.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react'
+import ApiService from '../../service/ApiService'
+import BookingResult from './BookingResult';
+
+const ManageBookingsPage = () => {
+
+    const [bookings, setBookings] = useState([]);
+
+    useEffect(() => {
+        const getBookings = async () => {
+            try{
+                const response = await ApiService.getAllBookings();
+                console.log("ALL BOOKINGS:", response);
+                setBookings(response.bookingList);
+
+            } catch (error) {
+                console.error('Error fetching bookings:', error.message);
+            }
+        };
+        getBookings();
+    }, []);
+    const handleDeleteBooking = async (bookingId/*, roomDescription*/) =>{
+        const isDelete = window.confirm(
+            `Are you sure you want to delete ${bookingId}?`
+        );
+
+        if(!isDelete) return;
+
+        try {
+            await ApiService.cancelBooking(bookingId);
+
+            setBookings(prevBookings => 
+                prevBookings.filter(booking => booking.id !== bookingId)
+            );
+        } catch (error) {
+            console.error("Error deleting booking:", error);
+        }
+    };
+  return (
+    <div>
+      <h2>Manage Bookings</h2>
+      <BookingResult bookings={bookings}
+      onDeleteBooking={handleDeleteBooking}/>
+    </div>
+  )
+}
+
+export default ManageBookingsPage
